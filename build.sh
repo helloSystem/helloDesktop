684710e6a3d1252fbdd36ef7d0ce47f6e95ed25d
probonopd <probonopd@users.noreply.github.com>
2022-11-12 23:21:37 +0100

    Try fixing Utilities

--- build.sh
+++ build.sh
@@ -49,9 +49,9 @@ ln -sf $(readlink -f ./sysutils/hellodesktop-qtplugin) /usr/ports/sysutils/
 # Utilities
 # Try prevent it from compiling python packages...
 # pkg install -y databases/py-sqlite3 devel/py-dateutil devel/py-pip devel/py-pyelftools devel/py-pytz devel/py-qt5-pyqt devel/py-xattr devel/py-xdg devel/py-xmltodict python3 sysutils/py-psutil www/py-beautifulsoup www/py-qt5-webengine
-# ( cd sysutils/hellodesktop-utilities && make build-depends-list | cut -c 12- | xargs pkg install -y )
-# make -C sysutils/hellodesktop-utilities package
-# ln -sf $(readlink -f ./sysutils/hellodesktop-utilities) /usr/ports/sysutils/
+( cd sysutils/hellodesktop-utilities && make build-depends-list | cut -c 12- | xargs pkg install -y )
+make -C sysutils/hellodesktop-utilities package
+ln -sf $(readlink -f ./sysutils/hellodesktop-utilities) /usr/ports/sysutils/
 
 # helloDesktop meta port
 make -C x11-wm/hellodesktop package
