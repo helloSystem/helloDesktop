b9da2e00d8d300d1270c958637e6668ffec67f77
probonopd <probonopd@users.noreply.github.com>
2022-11-13 00:40:43 +0100

    pyver=39 # our ports tree is outdated on Cirrus, hence specifying it by hand

--- build.sh
+++ build.sh
@@ -48,7 +48,8 @@ ln -sf $(readlink -f ./sysutils/hellodesktop-qtplugin) /usr/ports/sysutils/
 
 # Utilities
 # Try to prevent it from compiling python packages...
-pyver=$(cat /usr/ports/Mk/bsd.default-versions.mk | grep ^PYTHON_DEFAULT | cut -d "=" -f 2 | xargs | sed -e 's|\.||g')
+# pyver=$(cat /usr/ports/Mk/bsd.default-versions.mk | grep ^PYTHON_DEFAULT | cut -d "=" -f 2 | xargs | sed -e 's|\.||g')
+pyver=39 # our ports tree is outdated on Cirrus, hence specifying it by hand
 py=py$pyver
 pkg install -y $py-sqlite3 $py-dateutil $py-pyelftools $py-pytz $py-qt5-pyqt $py-xattr $py-xdg $py-xmltodict $py-psutil $py-beautifulsoup $py-qt5-webengine python$pyver
 ( cd sysutils/hellodesktop-utilities && make build-depends-list | cut -c 12- | xargs pkg install -y )
