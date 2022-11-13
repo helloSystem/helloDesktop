e8fffa8ef9910e46bbd810f421919d6e2ef9a004
probonopd <probonopd@users.noreply.github.com>
2022-11-14 00:21:27 +0100

    Update launch; don't build Utilities

--- build.sh
+++ build.sh
@@ -48,13 +48,13 @@ ln -sf $(readlink -f ./sysutils/hellodesktop-qtplugin) /usr/ports/sysutils/
 
 # Utilities
 # Try to prevent it from compiling python packages...
-# pyver=$(cat /usr/ports/Mk/bsd.default-versions.mk | grep ^PYTHON_DEFAULT | cut -d "=" -f 2 | xargs | sed -e 's|\.||g')
-pyver=39 # our ports tree is outdated on Cirrus, hence specifying it by hand
-py=py$pyver
-pkg install -y $py-sqlite3 $py-dateutil $py-pyelftools $py-pytz $py-qt5-pyqt $py-xattr $py-xdg $py-xmltodict $py-psutil $py-beautifulsoup $py-qt5-webengine python$pyver
-( cd sysutils/hellodesktop-utilities && make build-depends-list | cut -c 12- | xargs pkg install -y )
-make -C sysutils/hellodesktop-utilities package
-ln -sf $(readlink -f ./sysutils/hellodesktop-utilities) /usr/ports/sysutils/
+## pyver=$(cat /usr/ports/Mk/bsd.default-versions.mk | grep ^PYTHON_DEFAULT | cut -d "=" -f 2 | xargs | sed -e 's|\.||g')
+#pyver=39 # our ports tree is outdated on Cirrus, hence specifying it by hand
+#py=py$pyver
+#pkg install -y $py-sqlite3 $py-dateutil $py-pyelftools $py-pytz $py-qt5-pyqt $py-xattr $py-xdg $py-xmltodict $py-psutil $py-beautifulsoup $py-qt5-webengine python$pyver
+#( cd sysutils/hellodesktop-utilities && make build-depends-list | cut -c 12- | xargs pkg install -y )
+#make -C sysutils/hellodesktop-utilities package
+#ln -sf $(readlink -f ./sysutils/hellodesktop-utilities) /usr/ports/sysutils/
 
 # helloDesktop meta port
 make -C x11-wm/hellodesktop package
